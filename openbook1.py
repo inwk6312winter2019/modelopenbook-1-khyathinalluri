8ae4f747db4caf914b5a627706cbd403c6503adb
Khyathinalluri <Khyathi.Nalluri@dal.ca>
2019-02-06 08:55:10 -0800

    opening file contents

--- openbook1.py
+++ openbook1.py
@@ -1,14 +1,18 @@
-def list_ipname_ip(interface,name):
+def list_ipname_ip(address,netmask,name):
 	d={}
-	ipaddress,netmask=interface.split(" ")
-	d[name]=(ipaddress,netmask)
-	print(d)
+	d[name]=(address,netmask)
+	return d
 
+l=[]
 fout=open("running-config.cfg")
 for interface in fout:
-	if interface!= "!":
-		
-	else:
-		continue 
-
-
+	l.append(interface.split())
+	#print(l)
+for i in range(len(l)):
+	if l[i] =="nameif":
+		name=l[i+2]
+	elif l[i] == "ip":
+		address=l[i+3]
+		netwmask=l[i++5]
+		print(list_ipname_ip(address,netmask,name)) 
+	
