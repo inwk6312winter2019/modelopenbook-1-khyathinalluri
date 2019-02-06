8248430f964bc4d18df3d48c7638b68bf96587dd
Khyathinalluri <Khyathi.Nalluri@dal.ca>
2019-02-06 10:21:36 -0800

    2nd program

--- openbook1.py
+++ openbook1.py
@@ -3,10 +3,17 @@ def list_ipname_ip(address,netmask,name):
 	d[name]=(address,netmask)
 	return d
 
+def interchanging_mask(d):
+	for i in range(len(d)):
+		val=d[i][0]
+		val1=d[i][1]
+		if val == 172 or val== 192:
+			val.replace(val,"10")
+			val1.replace(val1,"255.0.0.0")
+	return d
+
+
 l=[]
-name=[]
-add=[]
-netmask=[]
 fout=open("running-config.cfg")
 for interface in fout:
 	l.append(interface.strip().split(" "))
@@ -18,4 +25,5 @@ for i in range(len(l)):
 		elif l[i][0]=="ip":
 			add=l[i][2]
 			netmask=l[i][3]
-			print(list_ipname_ip(add,netmask,name))
+			d=list_ipname_ip(add,netmask,name)
+			print(interchanging_mask(d))
